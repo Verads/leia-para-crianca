ebff031599886aca0ccc11702fc5a52160d41968
Verads <vds.verasantana@gmail.com>
2021-07-10 09:26:38 -0400

    Fixing the input search

--- src/pages/books/books.jsx
+++ src/pages/books/books.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react'
 
 import Cards from '../../components/cards/cards'
 import Footer from '../../components/footer/footer'
-import Input from '../../components/input/input'
 import Navbar from '../../components/navigation/Nav'
+import Search from '../../assets/search.svg'
 
 import './books.css'
 
@@ -11,32 +11,49 @@ import api from '../../services/api'
 
 function Books() {
 
-    const [ books, setBooks ] = useState([])
+    const [books, setBooks] = useState([])
+    const [search, setSearch] = useState('')
+    const [filtering, setFiltering] = useState([])
 
 
     useEffect(() => {
-        async function loadBooks(){
+        async function loadBooks() {
             const response = await api.get('/books/')
             const data = await response.data
             setBooks(data)
-            
+
         }
         loadBooks()
     }, [])
+
+    useEffect(() => {
+        setFiltering(
+            books.filter(book => {
+                return book.name.includes(search) || book.author.includes(search)
+            })
+        )
+    }, [search, books])
 
     return (
         <>
             <Navbar />
             <main className="main__books">
-                <Input />
-                <div className="main__books-cards">
-                {books.map(book => {
-                    return(
-                        <Cards title={book.name} author={book.author} key={book.id} age={book.indicated} />
-                    )
-                })}
+                <div className="search-books">
+                    <div className="search-books__container">
+                        <input className="search-books__input" type="text" placeholder="Procure pelo nome do livro..." onChange={e => { setSearch(e.target.value) }} />
+                        <button className="search-books_btn-img" >
+                            <img src={Search} alt="" />
+                        </button>
+                    </div>
                 </div>
-                
+                <div className="main__books-cards">
+                    {filtering.map(book => {
+                        return (
+                            <Cards title={book.name} author={book.author} key={book.id} age={book.indicated} />
+                        )
+                    })}
+                </div>
+
             </main>
             <Footer />
         </>
